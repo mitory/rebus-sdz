6a0e6c0b30659afb19653a20396217368a34b133
mitory <lairziro@gmail.com>
2024-01-24 15:01:28 +0300

    first_commit

--- js/burger.js
+++ js/burger.js
@@ -2,19 +2,46 @@ document.addEventListener("DOMContentLoaded", function () {
     var navMobileToggle = document.getElementById("nav-mobile-toggle");
     var navMobileMenu = document.getElementById("nav-mobile-menu");
     var navMobileClose = document.getElementById("nav-mobile-close");
+    var wrapper = document.getElementById("wrapper");
 
     navMobileToggle.addEventListener("click", function () {
         navMobileMenu.classList.toggle("show");
+        wrapper.classList.toggle("show");
     });
 
     navMobileClose.addEventListener("click", function () {
         navMobileMenu.classList.remove("show");
+        wrapper.classList.remove("show");
     });
 
     var navListItems = document.querySelectorAll(".nav__list.mobile .nav__item a");
     navListItems.forEach(function (item) {
         item.addEventListener("click", function () {
             navMobileMenu.classList.remove("show");
+            wrapper.classList.remove("show");
         });
     });
+
+    wrapper.addEventListener("click", function(event) {
+        wrapper.classList.remove("show");
+        navMobileMenu.classList.remove("show");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".nav");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+        header.style.background = getComputedStyle(document.documentElement).getPropertyValue('--main-green-color')
+        document.querySelector('.nav-mobile__logo').style.display = 'block';
+        document.querySelector('.nav-mobile__tel').style.display = 'block';
+        // header.style.background = "0";
+    } else {
+        document.querySelector('.nav-mobile__logo').style.display = 'none';
+        document.querySelector('.nav-mobile__tel').style.display = 'none';
+        header.style.background = 'transparent';
+        // header.style.top = "-100%"; 
+    }
+  });
 });
--- js/modal_form.js
+++ js/modal_form.js
@@ -5,16 +5,19 @@ var closeModalBtn = document.getElementById("closeModalBtn");
 
 openModalBtn.addEventListener("click", function() {
     modal.style.display = "block";
+    document.querySelector(".nav").style.display = 'none'
 });
 
 closeModalBtn.addEventListener("click", function() {
     modal.style.display = "none";
+    document.querySelector(".nav").style.display = 'block'
     form_modal.reset();
 });
 
 window.addEventListener("click", function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        document.querySelector(".nav").style.display = 'block'
         form_modal.reset();
     }
 });
